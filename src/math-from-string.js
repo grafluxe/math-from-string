830c9de8817aa92a24c139c76afe43facbc12717
Leandro Silva <leandro@grafluxe.com>
2018-04-06 16:18:26 -0400

    Improve regex logic

--- src/math-from-string.js
+++ src/math-from-string.js
@@ -2,36 +2,31 @@
  * @author Leandro Silva
  * @copyright 2014, 2017-2018 Leandro Silva (http://grafluxe.com)
  * @license MIT
- *
- * @desc Parses a string as a mathematical expression. Supports addition,
- *       subtraction, division, and multiplication.
- * @throws  {Error} Spaces and letters are not allowed.
- * @throws  {Error} Your string has two consecutive operators.
- * @throws  {Error} Your string is malformed.
- * @param   {String} str The string to parse.
- * @returns {Number} The end value.
  */
 
-let notAllowed = /[A-Za-z\s]/,
-    consecutiveOps = /[+\-*/]{3,}/,
-    malformedStr = /[\\*+-]$|^[\\*+]/,
+let expectedStr = /^[\d-(.][\d+\-*/().]+[\d)]$/,
+    unexpectedOps = /(?!\*\*)[+\-*/][+/*]\d|[+\-*/]{3,}/,
     parens = /\(([^(]+?)\)/,
-    hasMulDiv = /\/|\*/,
-    mulDiv = /(-?\d+)([/*])(-?\d+)/,
+    hasMulDiv = /[/*]/,
+    mulDiv = /(-?[\d.]+)([/*]*?)(-?[\d.]+)/,
     hasAddSub = /(?!^-)-|\+/,
-    addSub = /(-?\d+)([+-])(-?\d+)/;
+    addSub = /(-?[\d.]+)([+-])(-?[\d.]+)/;
 
+/**
+ * @desc Parses a string as a mathematical expression. Supports addition,
+ * subtraction, division, and multiplication.
+ * @throws	{Error}  The string at/near "<value>" is malformed.
+ * @throws	{Error}  The string at/near "<value>" has a malformed operator.
+ * @param   {String} str The string to parse.
+ * @returns {Number} The end total.
+ */
 function mathFromString(str) {
-  if (notAllowed.test(str)) {
-    throw new Error("Spaces and letters are not allowed.");
+  if (!expectedStr.test(str)) {
+    throw new Error(`The string at/near "${str}" is malformed.`);
   }
 
-  if (consecutiveOps.test(str)) {
-    throw new Error("Your string has two consecutive operators.");
-  }
-
-  if (malformedStr.test(str)) {
-    throw new Error("Your string is malformed.");
+  if (unexpectedOps.test(str)) {
+    throw new Error(`The string at/near "${str}" has a malformed operator.`);
   }
 
   // Do math inside parentheses first
